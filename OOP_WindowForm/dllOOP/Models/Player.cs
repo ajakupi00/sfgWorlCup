986ea314fd38aa111a6d1aabf7a83b198d910d39
ajakupi <arjan.jakupi007@gmail.com>
2022-06-23 06:02:31 +0200

    Player class

---
+++ OOP_WindowForm/dllOOP/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dllOOP.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public bool Captain { get; set; }
+        public int Shirt_Number { get; set; }
+        public string Position { get; set; }
+    }
+}

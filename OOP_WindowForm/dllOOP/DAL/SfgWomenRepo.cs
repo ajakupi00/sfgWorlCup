ebdcd8e3318ea067ce02017141eced7877cdbbc6
ajakupi <arjan.jakupi007@gmail.com>
2022-06-24 05:48:13 +0200

    Interface implemented and endpoints set

--- OOP_WindowForm/dllOOP/DAL/SfgWomenRepo.cs
+++ OOP_WindowForm/dllOOP/DAL/SfgWomenRepo.cs
@@ -1,5 +1,6 @@
 ﻿using dllOOOP.Models;
 using dllOOP.Models;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -10,21 +11,63 @@ using System.Threading.Tasks;
 namespace dllOOP.DAL
 {
     public class SfgWomenRepo : ISfg
-
     {
+        private const string NATIONAL_TEAMS_ENDPOINT = @"https://worldcup.sfg.io/teams/results";
+        private const string MATCH_ENDPOINT = @"https://worldcup.sfg.io/matches/country?fifa_code=";
+
+
+        public static List<T> DeserializeObject<T>(RestResponse<T> odgovorPodaci)
+        {
+            return (List<T>)JsonConvert.DeserializeObject(odgovorPodaci.Content, typeof(List<T>));
+        }
+
         public Task<RestResponse<Match>> GetMatches(NationalTeam team)
         {
-            throw new NotImplementedException();
+            var apiKlijent = new RestClient(MATCH_ENDPOINT + team.FifaCode);
+            return apiKlijent.ExecuteAsync<Match>(new RestRequest());
         }
 
         public Task<RestResponse<NationalTeam>> GetNationalTeams()
         {
-            throw new NotImplementedException();
+            var apiKlijent = new RestClient(NATIONAL_TEAMS_ENDPOINT);
+            return apiKlijent.ExecuteAsync<NationalTeam>(new RestRequest());
         }
 
         public async Task<HashSet<Player>> GetPlayers(NationalTeam team)
         {
-            throw new NotImplementedException();
+            RestResponse<Match> response = await GetMatches(team);
+            List<Match> matches = SfgMenRepo.DeserializeObject(response);
+            HashSet<Player> players = new HashSet<Player>();
+
+            foreach (Match match in matches)
+            {
+                if (match.HomeTeamCountry == team.Country)
+                {
+                    for (int i = 0; i < match.HomeTeamStatistics.StartingEleven.Count; i++)
+                    {
+                        players.Add(match.HomeTeamStatistics.StartingEleven[i]);
+                    }
+                    for (int i = 0; i < match.HomeTeamStatistics.Substitutes.Count; i++)
+                    {
+                        players.Add(match.HomeTeamStatistics.Substitutes[i]);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < match.AwayTeamStatistics.StartingEleven.Count; i++)
+                    {
+                        players.Add(match.AwayTeamStatistics.StartingEleven[i]);
+                    }
+                    for (int i = 0; i < match.AwayTeamStatistics.Substitutes.Count; i++)
+                    {
+                        players.Add(match.AwayTeamStatistics.Substitutes[i]);
+                    }
+                }
+
+            }
+
+            return players;
         }
+
     }
 }

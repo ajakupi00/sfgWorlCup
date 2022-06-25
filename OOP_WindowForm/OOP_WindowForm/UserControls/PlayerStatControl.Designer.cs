5cdaf4c81bce619dff7a59c3ba3d6e40373172d9
ajakupi <arjan.jakupi007@gmail.com>
2022-06-25 07:53:57 +0200

    Player stat control

---
+++ OOP_WindowForm/OOP_WindowForm/UserControls/PlayerStatControl.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace OOP_WindowForm.UserControls
+{
+    partial class PlayerStatControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayerStatControl));
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblGoals = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCards = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Image = ((System.Drawing.Image)(resources.GetObject("pbImage.Image")));
+            this.pbImage.Location = new System.Drawing.Point(17, 16);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(123, 127);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblName.Location = new System.Drawing.Point(146, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(198, 27);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "BIGFIRST BIGLAST";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblGoals
+            // 
+            this.lblGoals.AutoSize = true;
+            this.lblGoals.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGoals.Location = new System.Drawing.Point(223, 67);
+            this.lblGoals.Name = "lblGoals";
+            this.lblGoals.Size = new System.Drawing.Size(30, 23);
+            this.lblGoals.TabIndex = 2;
+            this.lblGoals.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(152, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Goals:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(152, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yellow cards:";
+            // 
+            // lblCards
+            // 
+            this.lblCards.AutoSize = true;
+            this.lblCards.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCards.Location = new System.Drawing.Point(294, 96);
+            this.lblCards.Name = "lblCards";
+            this.lblCards.Size = new System.Drawing.Size(30, 23);
+            this.lblCards.TabIndex = 4;
+            this.lblCards.Text = "00";
+            // 
+            // PlayerStatControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCards);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblGoals);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pbImage);
+            this.Name = "PlayerStatControl";
+            this.Size = new System.Drawing.Size(399, 163);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblGoals;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCards;
+    }
+}
---
+++ OOP_WindowForm/OOP_WindowForm/UserControls/PlayerStatControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_WindowForm.UserControls
+{
+    public partial class PlayerStatControl : UserControl
+    {
+        private string playerName;
+        private string picturePath;
+        private int goals;
+        private int cards;
+        public PlayerStatControl()
+        {
+            InitializeComponent();
+        }
+
+        public string PlayerName { 
+            get => playerName;
+            set {
+                playerName = value;
+                lblName.Text = value;
+            }
+        }
+        public int Goals { 
+            get => goals;
+            set {
+                goals = value;
+                lblGoals.Text = value.ToString();
+            } 
+        }
+        public int Cards { 
+            get => cards; 
+            set {
+                cards = value;
+                lblCards.Text = value.ToString();
+            } 
+        }
+
+        public string PicturePath { 
+            get => picturePath;
+            set {
+                picturePath = value;
+                pbImage.Image = Image.FromFile(value);
+            }
+        }
+    }
+}

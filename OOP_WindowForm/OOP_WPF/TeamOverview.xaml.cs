1cbc32256eff92a767d1f2f8f191d2d9e62d8151
ajakupi <arjan.jakupi007@gmail.com>
2022-06-26 09:55:33 +0200

    Matche goals displayed async

--- OOP_WindowForm/OOP_WPF/TeamOverview.xaml.cs
+++ OOP_WindowForm/OOP_WPF/TeamOverview.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using dllOOP.DAL;
 using dllOOP.DAL.Interfaces;
+using dllOOP.Models;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,14 +24,67 @@ namespace OOP_WPF
     public partial class TeamOverview : Window
     {
         private IRepo repo = RepoFactory.GetRepo();
+        private List<Match> matchesOfTeam = new List<Match>();
+
+        private ISfg sfg;
+        private NationalTeam favTeam;
+        private NationalTeam opponentTeam;
         public TeamOverview()
         {
             InitializeComponent();
             InitSettings();
+            InitCombos();
+        }
+
+        private async void InitCombos()
+        {
+            lblFavTeam.Content = "";
+            lblGoalFav.Content = "";
+            lblTeam.Content = "";
+            lblGoalTeam.Content = "";
+            lblDots.Content = "";
+            lblScore.Content = "Loading countires..";
+            RestResponse<NationalTeam> odgovorPodaci = await sfg.GetNationalTeams();
+            List<NationalTeam> teams = SfgMenRepo.DeserializeObject(odgovorPodaci);
+
+            RestResponse<Match> response = await sfg.GetMatches(favTeam);
+            List<Match> matches = SfgMenRepo.DeserializeObject(response);
+            matchesOfTeam = matches;
+            cbFavNation.Items.Clear();
+            foreach (NationalTeam team in teams)
+            {
+                cbFavNation.Items.Add(team);
+
+                if (team.FifaCode == favTeam.FifaCode)
+                {
+                    cbFavNation.SelectedIndex = cbFavNation.Items.Count - 1;
+                }
+
+            }
+            cbNation.Items.Clear();
+
+            foreach (NationalTeam team in teams)
+            {
+                if (team.FifaCode != favTeam.FifaCode)
+                {
+                    foreach (Match match in matches)
+                    {
+                        if (match.HomeTeamCountry == team.Country || match.AwayTeamCountry == team.Country)
+                            cbNation.Items.Add(team);
+                    }
+                }
+
+            }
+            lblScore.Content = "Choose opponent team";
+
+
+
         }
 
         private void InitSettings()
         {
+            sfg = SfgFactory.GetSfg(repo.GetSexSetting());
+            favTeam = repo.GetFavoriteTeam();
             string resx = repo.GetResolution();
             if (resx == "fullscreen")
             {
@@ -44,8 +99,49 @@ namespace OOP_WPF
                 this.Height = height;
                 this.Width = width;
             }
+
+
+        }
+
+        private void btnStat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbFavNation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            if (cb.IsEditable)
+            {
+                favTeam = (NationalTeam)cb.SelectedItem;
+                InitCombos();
+
+            }
+
+        }
+
+        private void cbNation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            if (cb.IsEditable)
+            {
+                opponentTeam = (NationalTeam)cb.SelectedItem;
+                UpdateScore();
+            }
             
-            
+        }
+
+        private async void UpdateScore()
+        {
+            RestResponse<Match> response = await sfg.GetMatches(favTeam);
+            List<Match> matches = SfgMenRepo.DeserializeObject(response);
+            Match match = matches.FirstOrDefault(m => (m.HomeTeamCountry == favTeam.Country && m.AwayTeamCountry == opponentTeam.Country) || (m.AwayTeamCountry == favTeam.Country && m.HomeTeamCountry == opponentTeam.Country));
+            lblScore.Content = "Score";
+            lblFavTeam.Content = favTeam.FifaCode;
+            lblGoalFav.Content = (match.HomeTeam.Country == favTeam.Country) ? match.HomeTeam.Goals : match.AwayTeam.Goals;
+            lblTeam.Content = opponentTeam.FifaCode;
+            lblGoalTeam.Content = (match.HomeTeam.Country == opponentTeam.Country) ? match.HomeTeam.Goals : match.AwayTeam.Goals;
+            lblDots.Content = ":";
         }
     }
 }
